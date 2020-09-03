277950f5acd2f3fc7240a4c22e18c333ccd7bf40
Lucas Silveira <lugano1995@gmail.com>
2020-09-03 00:45:38 -0300

    Fundamentals arithmetic

---
+++ Fundamentals/arithmetic.php
@@ -0,0 +1,12 @@
+<?php
+
+  var_dump(1 + 1); // int(2)
+  var_dump(1 + 1.0); // float(2)
+  var_dump(2 - 1); // int(1)
+  var_dump(2 * 5); // int(10)
+  var_dump(7 / 4); // float(1.75)
+  var_dump(intdiv(7, 4)); // int(1)
+  var_dump(round(7 / 4)); // float(2)
+  var_dump(7 % 4); // int(3)
+  var_dump(2 ** 4); // int(16)
+  print("\n");
