2eef56ee1a9adae0180f29cb20a14f0807711847
Lucas Silveira <lugano1995@gmail.com>
2020-09-04 20:42:32 -0300

    Structural Control switch

---
+++ Structural Control/switch.php
@@ -0,0 +1,23 @@
+<?php
+
+$category = 'A';
+
+switch($category) {
+  case 'A':
+    var_dump('its A!');
+    break;
+  case 'B':
+    var_dump('its B!');
+    break;
+  case 'C':
+    var_dump('its C!');
+    break;
+  case 'D':
+    var_dump('its D!');
+    break;
+  case 'E':
+    var_dump('its E!');
+    break;
+  default:
+    var_dump('its not anything');
+}
