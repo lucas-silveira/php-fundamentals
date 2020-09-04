e78d1b8b9134d26829219ceeeafae91a14de6300
Lucas Silveira <lugano1995@gmail.com>
2020-09-04 20:20:02 -0300

    Structural Control if else

---
+++ Structural Control/ifelse.php
@@ -0,0 +1,28 @@
+<?php
+
+if (true)
+  var_dump("verdadeiro");
+
+if (false) {
+  var_dump("falso");
+}
+
+if (true)
+  var_dump("verdadeiro");
+else
+  var_dump("falso");
+
+if (false) {
+  var_dump("verdadeiro");
+} else {
+  var_dump("falso");
+}
+
+if (false) {
+  var_dump("passo a");
+} else if (true) {
+  var_dump("passo b");
+} else {
+  var_dump("passo c");
+}
+
