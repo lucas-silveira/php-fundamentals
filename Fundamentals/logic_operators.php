94cdd38122a2a0b3948f81bfbf74e90b9be879db
Lucas Silveira <lugano1995@gmail.com>
2020-09-04 20:36:22 -0300

    Fundamentals logic operators

---
+++ Fundamentals/logic_operators.php
@@ -0,0 +1,18 @@
+<?php
+
+  var_dump(true && true); // bool(true)
+  var_dump(true && false); // bool(false)
+  var_dump(false && true); // bool(false)
+  var_dump(false && false); // bool(false)
+  var_dump(true and true); // bool(true)
+  print(PHP_EOL);
+  var_dump(true || true); // bool(true)
+  var_dump(true || false); // bool(true)
+  var_dump(false || true); // bool(true)
+  var_dump(false || false); // bool(false)
+  var_dump(true or true); // bool(true)
+  print(PHP_EOL);
+  var_dump(true xor true); // bool(false)
+  var_dump(true xor false); // bool(true)
+  var_dump(false xor true); // bool(true)
+  var_dump(false xor false); // bool(false)
