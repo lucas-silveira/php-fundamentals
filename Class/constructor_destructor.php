14b72fd08e72891f4abc05a03b2d8d62a50c7597
Lucas Silveira <lugano1995@gmail.com>
2020-09-08 22:33:16 -0300

    class constructor and destructor

---
+++ Class/constructor_destructor.php
@@ -0,0 +1,22 @@
+<?php
+
+class Person {
+  public $name;
+  public $age;
+
+  public function __construct($name, $age) {
+    $this->name = $name;
+    $this->age = $age;
+  }
+
+  public function __destruct() {
+    print("Fim do objeto\n");
+  }
+
+  public function sayHello() {
+    return "Olá. Sou o {$this->name}!\n";
+  }
+}
+
+$person = new Person('Peter Park', 20);
+print($person->sayHello()); // Olá. Sou o Peter Park!
