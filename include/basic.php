8428c21738acbd93c304c6dadcce4eeeca9a9804
Lucas Silveira <lugano1995@gmail.com>
2020-09-11 20:43:53 -0300

    include basic

---
+++ include/basic.php
@@ -0,0 +1,6 @@
+<?php
+
+print("Arquivo basic\n");
+include('sum.php');
+
+var_dump(sum(1, 2)); // int(3)
---
+++ include/sum.php
@@ -0,0 +1,9 @@
+<?php
+
+print("Carregando: sum.php\n");
+
+$variable = 'something';
+
+function sum($a, $b) {
+  return $a + $b;
+}
