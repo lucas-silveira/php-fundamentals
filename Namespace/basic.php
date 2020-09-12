c75aef1f7eef81723a955db8f36d1b2e412f358a
Lucas Silveira <lugano1995@gmail.com>
2020-09-11 21:17:29 -0300

    namespace basic

---
+++ Namespace/basic.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Context_Name;
+
+print(__NAMESPACE__); // Context_Name
+print(PHP_EOL);
+
+const CONSTANT1 = 123;
+define(__NAMESPACE__ . '\CONSTANTE2', 456);
+
+var_dump(CONSTANT1, CONSTANTE2);
+// int(123)
+// int(456)
+
+function sum($a, $b) {
+  return $a + $b;
+}
+
+var_dump(sum(1, 2)); // int(3)
+var_dump(\Context_Name\sum(2,2)); // int(4)
